eec60e3b1b0d1759bed78b56de0a647df2b0c0f0
Jigme Lodey <jigmenlodey@gmail.com>
2025-10-22 11:16:47 +0600

    fix: updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // helps catch potential issues
-  swcMinify: true,       // faster minification with SWC
   images: {
     domains: [
       "images.unsplash.com", // allow banner images from Unsplash
